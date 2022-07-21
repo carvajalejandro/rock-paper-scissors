1d24ee4d537b6eae6ec1f94fbcef0ad8773e31b5
carvajalejandro <alejandro.carvajal@mycase.com>
2022-07-20 20:29:54 -0700

    Write out comments on current plan for code

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 console.log("hello world")
+//Create a variable called computerChoice and set it equal to random of the three variables below
+//Create a variable called Rock and set it equal to "rock"
+//Create a variable called Paper and set it equal to "paper"
+//Create a variable called Scissors and set it equal to "scissors"
+//Create a function that randomly selects between these three options and call it getComputerChoice
+
+//Create a variable called playerChoice and and set it equal to random of the three variables below
+//Create a variable called Rock and set it equal to "rock"
+//Create a variable called Paper and set it equal to "paper"
+//Create a variable called Scissors and set it equal to "scissors"
+//Create a function that randomly selects between these three options and call it getPlayerChoice
+
+//Create a function that grabs computerChoice and compares it to playerChoice
+
+//If computerChoice equals rock && playerChoice equals rock, then display "It is a tie"
+//If computerChoice equals rock && playerChoice equals paper, then display "You win this round"
+//If computerChoice equals rock && playerChoice equals scissors, then display "You lose this round"
+
+//If computerChoice equals paper && playerChoice equals paper, then display "It is a tie"
+//If computerChoice equals paper && playerChoice equals scissors, then display "You win this round"
+//If computerChoice equals paper && playerChoice equals rock, then display "You lose this round"
+
+//If computerChoice equals scissors && playerChoice equals scissors, then display "It is a tie"
+//If computerChoice equals scissors && playerChoice equals rock, then display "You win this round"
+//If computerChoice equals scissors && playerChoice equals paper, then display "You lose this round"
