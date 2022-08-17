09b65d3cdd641e4da0b1b21c6e787f0003163359
carvajalejandro <alejandro.carvajal@mycase.com>
2022-08-17 15:32:03 -0700

    Functioning game, no way to increase player score with turnary operator.

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
+const gameDiv= document.querySelector("#gameDiv");
 const playerText = document.querySelector("#playerText")
 const computerText = document.querySelector("#computerText")
 const resultText = document.querySelector("#resultText")
 const choiceBtns = document.querySelectorAll(".choiceBtn");
+const playerWins = document.createElement('div');
 
+
+
+//Variables
 let player;
+let playerScore =0;
+let computerScore=0;
 let computer;
+let tieScore=0;;
 let result;
 
-
-
-
+//This causes a round to occur
 choiceBtns.forEach(button => button.addEventListener("click", () => {
     player= button.textContent;
     computerTurn();
     playerText.textContent = `Player: ${player}`;
     computerText.textContent = `Computer: ${computer}`;
-    resultText.textContent = checkWinner();
-    game();
+    resultText.textContent = playRound();
 
   }));
-
-let playerScore = 0;
-let compScore = 0;
 
 //this gets the computer choice
 function computerTurn() {
@@ -38,9 +40,10 @@ switch(randomNum){
         break;
     }
 }
-
-function checkWinner(){
+//This compares the choice of player and computer
+function playRound(){
     if (player==computer){
+        tieScore++;
         return 'Draw!'
     } else if(computer== 'Rock'){
         return (player=='Paper') ?'You Win!' :'You Lose!'
@@ -52,23 +55,20 @@ function checkWinner(){
         return (player=='Rock') ?'You Win!' :'You Lose!'
     } 
 }
-//this function compares player vs computer
 
-
-// this grabs
-const game= ()=>{
-    for(let i=0; i < 5; i++ ){
-        const computerSelection = computerTurn();
-        const playerSelection = button.textContent;
+// const game= ()=>{
+//     for(let i=0; i < 5; i++ ){
+//         const computerSelection = computerTurn();
+//         const playerSelection = button.textContent;
       
 
-    if (playerScore>compScore){
-        return 'You beat the computer'
-    } else if (playerScore<compScore){
-        return 'You lost to the computer'
-    } else{
-        return 'It is a tie!'
-    }
-    }
-}   
-// console.log(game(player))
+//     if (playerScore>compScore){
+//         return 'You beat the computer'
+//     } else if (playerScore<compScore){
+//         return 'You lost to the computer'
+//     } else{
+//         return 'It is a tie!'
+//     }
+//     }
+// }   
+
