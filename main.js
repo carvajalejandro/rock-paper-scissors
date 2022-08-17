1aaac52220f2861bd7022915149c29e4a78a808a
carvajalejandro <alejandro.carvajal@mycase.com>
2022-08-16 20:52:35 -0700

    re-code game up to set player selection is rock

--- main.js
+++ main.js
@@ -1,90 +1,55 @@
-/*
-//Create variables for winning losing and ties
-let win=0
-let loss=0
-let tie=0
-//Create a loop that increases that increase the value of i every time that result displays via alert
-for (let i = 0; i < 5; i++) {
-//Create a variable called computerChoice and set it equal to random of the three variables below
-let computerChoice = Math.floor(Math.random()*3+1);
-console.log (computerChoice);
-//Create a function that takes the random number and changes the value to a string
-function getComputerChoice(){
-    if (computerChoice===1){
-        computerChoice= "rock"
-    } else if (computerChoice===2){
-        computerChoice= "paper"
-    } else if (computerChoice===3){
-        computerChoice= "scissors"
-}
-}
-//Create a variable called playerChoice and ask the user for a value
-let playerInput= window.prompt("Choose Rock, Paper, or Scissors.")
-//Create a variable that takes the data from the user and turn into all lower case string
-let playerChoice= playerInput.toLowerCase()
-console.log (playerChoice);
-let result;
-//Create a function that grabs computerChoice and compares it to playerChoice
-function playRound(){
-if (computerChoice==="rock" && playerChoice==="rock"){
-    result= "It is a tie"; 
-    tie ++;
-} else if (computerChoice==="rock" && playerChoice==="paper"){
-    result="You win";
-    win++;
-} else if (computerChoice==="rock" && playerChoice==="scissors"){
-    result="You lose";
-    loss++;
-} else if (computerChoice==="paper" && playerChoice==="paper"){
-    result= "It is a tie";
-    tie++;
-} else if (computerChoice==="paper" && playerChoice==="scissors"){
-    result="You win";
-    win++;
-} else if (computerChoice==="paper" && playerChoice==="rock"){
-    result="You lose";
-    loss++;
-} else if (computerChoice==="scissors" && playerChoice==="scissors"){
-    result= "It is a tie";
-    tie++;
-} else if (computerChoice==="scissors" && playerChoice==="rock"){
-    result="You win";
-    win++;
-} else if (computerChoice==="scissors" && playerChoice==="paper"){
-    result="You lose";
-    loss++;
-} else {
-    result="Please choose between Rock, Paper, and Scissors"
-}
+let playerScore = 0;
+let compScore = 0;
+
+
+const computerPlay=() => {
+const arrOfChoices = ['rock','paper', 'scissors']
+const randomNum= Math.floor(Math.random()*3)
+const compChoice= arrOfChoices[randomNum];
+return compChoice;
 }
 
-//Create a loop that adds values to win, loss, & tie to keep score
-//Create a function called game that makes playRound occur
-function game(){
-    getComputerChoice();
-    playRound(); 
+const playRound = (playerSelection, computerSelection) =>{
+    if (playerSelection==='rock' && computerSelection==='rock'){
+        return 'You tied! You both chose rock'
+    } else  if (playerSelection==='paper' && computerSelection==='paper'){
+        return 'You tied! You both chose paper'
+    } else if (playerSelection==='scissors' && computerSelection==='scissors'){
+        return 'You tied! You both chose scissors'
+    } else if (playerSelection==='rock' && computerSelection==='scissors'){
+        playerScore++
+        return 'You win! Rock crushes Scissors'
+    } else if (playerSelection==='paper' && computerSelection==='rock'){
+        playerScore++
+        return 'You win! Paper wraps Rock'
+    } else if (playerSelection==='scissors' && computerSelection==='paper'){
+        playerScore++
+        return 'You win! Scissors cut Paper'
+    } else if (playerSelection==='scissors' && computerSelection==='rock'){
+        compScore++
+        return 'You lose! Rock crushes Scissors';
+    } else if (playerSelection==='paper' && computerSelection==='scissors'){
+        compScore++
+        return 'You lose! Scissors cut Paper'
+    } else if (playerSelection==='rock' && computerSelection==='paper'){
+        compScore++
+        return 'You lose! Paper wraps Rock'
+        
+    }   
+};
+
+const playerSelection = 'rock';
+const game= ()=>{
+    for(let i=0; i < 5; i++ ){
+        const computerSelection = computerPlay();
+        playRound(playerSelection,computerSelection);
     }
-
-game();
-console.log(result);
-console.log (win,loss,tie);
-}
-*/
-
-//Testing Buttons
-
-let container = document.querySelector('#container');
-let btnRock = document.createElement("button");
-btnRock.classList.add('rock');
-btnRock.textContent='Rock';
-container.appendChild(btnRock);
-
-let btnPaper = document.createElement("button");
-btnPaper.classList.add('paper');
-btnPaper.textContent='Paper';
-container.appendChild(btnPaper);
-
-let btnSci = document.createElement("button");
-btnSci.classList.add('rock');
-btnSci.textContent='Rock';
-container.appendChild(btnSci);
+    if (playerScore>compScore){
+        return 'You beat the computer'
+    } else if (playerScore<compScore){
+        return 'You lost to the computer'
+    } else{
+        return 'You tied!'
+    }  
+  }
+console.log(game());
